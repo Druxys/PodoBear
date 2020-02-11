118458dd23cdbeaf55a45d986470649866ee091b
Druxys <drudrux@gmail.com>
2020-02-11 12:30:40 +0100

    page register

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -15,6 +15,10 @@ const routes: Routes = [
     path: 'sign-in',
     loadChildren: () => import('./sign-in/sign-in.module').then( m => m.SignInPageModule)
   },
+  {
+    path: 'sign-up',
+    loadChildren: () => import('./sign-up/sign-up.module').then( m => m.SignUpPageModule)
+  },
 
 ];
 
