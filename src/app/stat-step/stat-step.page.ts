81371e57e4f2a3442514ba3ba38ee2ee749e3678
Druxys <drudrux@gmail.com>
2020-02-27 14:53:12 +0100

    add stats page for the steps

---
+++ src/app/stat-step/stat-step.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-stat-step',
+  templateUrl: './stat-step.page.html',
+  styleUrls: ['./stat-step.page.scss'],
+})
+export class StatStepPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
