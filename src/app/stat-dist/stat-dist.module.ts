c476144e09c9ac13bfafb93dc5c16027bf02a444
Druxys <drudrux@gmail.com>
2020-03-03 16:41:35 +0100

    add distance stats page

---
+++ src/app/stat-dist/stat-dist.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { IonicModule } from '@ionic/angular';
+
+import { StatDistPageRoutingModule } from './stat-dist-routing.module';
+
+import { StatDistPage } from './stat-dist.page';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    StatDistPageRoutingModule
+  ],
+  declarations: [StatDistPage]
+})
+export class StatDistPageModule {}
