902c56668c48de453db47f7c1b68c4502b355311
Druxys <drudrux@gmail.com>
2020-03-08 15:51:38 +0100

    device uuid don't with capacitor we need to remigrate to cordova

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,14 +14,25 @@ import {StatDistPageModule} from './stat-dist/stat-dist.module';
 import {SettingsPageModule} from './settings/settings.module';
 import {StatKalPageModule} from './stat-kal/stat-kal.module';
 
+import {Gyroscope, GyroscopeOrientation, GyroscopeOptions} from '@ionic-native/gyroscope/ngx';
+import {DeviceMotion, DeviceMotionAccelerationData} from '@ionic-native/device-motion/ngx';
+import {HttpClientModule, HttpHeaders} from '@angular/common/http';
+import {Geolocation} from '@ionic-native/geolocation/ngx';
+import {Device} from '@ionic-native/device/ngx';
+
+
 @NgModule({
     declarations: [AppComponent],
     entryComponents: [],
-    imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, StatStepPageModule, StatDistPageModule, SettingsPageModule, StatKalPageModule],
+    imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, StatStepPageModule, StatDistPageModule, SettingsPageModule, StatKalPageModule, HttpClientModule,],
     providers: [
         StatusBar,
         SplashScreen,
-        {provide: RouteReuseStrategy, useClass: IonicRouteStrategy}
+        {provide: RouteReuseStrategy, useClass: IonicRouteStrategy},
+        Geolocation,
+        Device,
+        Gyroscope,
+        DeviceMotion,
     ],
     bootstrap: [AppComponent]
 })
