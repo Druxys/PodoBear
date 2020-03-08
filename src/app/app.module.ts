bf976a41553c96b1928c2fabf5a0a3badb4a6cdf
Druxys <drudrux@gmail.com>
2020-03-08 10:01:37 +0100

    improving home + settings

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,29 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { RouteReuseStrategy } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {RouteReuseStrategy} from '@angular/router';
 
-import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
+import {IonicModule, IonicRouteStrategy} from '@ionic/angular';
+import {SplashScreen} from '@ionic-native/splash-screen/ngx';
+import {StatusBar} from '@ionic-native/status-bar/ngx';
 
-import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
+import {AppComponent} from './app.component';
+import {AppRoutingModule} from './app-routing.module';
 
-import  { StatStepPageModule } from './stat-step/stat-step.module';
-import  { StatDistPageModule } from './stat-dist/stat-dist.module';
+import {StatStepPageModule} from './stat-step/stat-step.module';
+import {StatDistPageModule} from './stat-dist/stat-dist.module';
 import {SettingsPageModule} from './settings/settings.module';
-
+import {StatKalPageModule} from './stat-kal/stat-kal.module';
 
 @NgModule({
-  declarations: [AppComponent],
-  entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, StatStepPageModule, StatDistPageModule, SettingsPageModule],
-  providers: [
-    StatusBar,
-    SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
-  ],
-  bootstrap: [AppComponent]
+    declarations: [AppComponent],
+    entryComponents: [],
+    imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, StatStepPageModule, StatDistPageModule, SettingsPageModule, StatKalPageModule],
+    providers: [
+        StatusBar,
+        SplashScreen,
+        {provide: RouteReuseStrategy, useClass: IonicRouteStrategy}
+    ],
+    bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule {
+}
--- src/app/stat-kal/stat-kal.page.ts
+++ src/app/stat-kal/stat-kal.page.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, Input, ViewChild} from '@angular/core';
+import {ModalController, NavParams} from '@ionic/angular';
+import {Chart} from 'chart.js';
 
 @Component({
   selector: 'app-stat-kal',
@@ -7,7 +9,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StatKalPage implements OnInit {
 
-  constructor() { }
+  // @ts-ignore
+  @ViewChild('barChart') barChart;
+
+  bars: any;
+  colorArray: any;
+
+
+  constructor(navParams: NavParams, public viewCtrl: ModalController) {
+  }
+
+  dismiss() {
+    this.viewCtrl.dismiss();
+  }
+
+  ionViewDidEnter() {
+    this.createBarChart();
+  }
+
+  createBarChart() {
+    this.bars = new Chart(this.barChart.nativeElement, {
+      type: 'bar',
+      data: {
+        labels: [
+          'L',
+          'M',
+          'M',
+          'J',
+          'V',
+          'S',
+          'D'
+        ],
+        datasets: [{
+          data: [10, 20, 30, 40, 50, 60, 70],
+        }],
+
+
+
+      },
+      options: {
+        legend: {
+          display: false
+        }}
+    });
+  }
 
   ngOnInit() {
   }
