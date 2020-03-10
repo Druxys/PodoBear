4114e80fd0fe2abdcfe2c2736c6ea67340aeb7d1
17359 <camille.lupo@hotmail.fr>
2020-03-10 09:49:30 +0100

    ajout sign-in/sign-up

--- src/app/Pages/sign-in/sign-in.page.ts
+++ src/app/Pages/sign-in/sign-in.page.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { AuthService} from '../../Service/auth.service';
-import { Router } from '@angular/router';
+import {Router} from '@angular/router';
+import {AuthService} from '../../Service/auth.service';
+import {AlertService} from '../../Service/alert.service';
+import {Validators, FormBuilder, FormGroup } from '@angular/forms';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-sign-in',
@@ -9,12 +11,53 @@ import { Router } from '@angular/router';
   styleUrls: ['./sign-in.page.scss'],
 })
 export class SignInPage implements OnInit {
-  register: FormGroup;
+  form: FormGroup;
   submitted = false;
   loading = false;
-  constructor() { }
+
+
+  constructor(private authService: AuthService,
+              private router: Router,
+              private alert: AlertService,
+              private fb: FormBuilder,
+              private alertController: AlertController
+  ) {
+    this.form = this.fb.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+  get f() {
+    return this.form.controls;
+  }
+
+  async login() {
+    this.submitted = true;
+    const val = this.form.value;
+    if (val.email && val.password) {
+      this.loading = true;
+      const resultConnection = await this.authService.login(this.form.value, (err) => {
+        if (err) {
+          this.presentAlert(err);
+        }
+      }).then();
+    }
+  }
 
   ngOnInit() {
   }
 
+  movetosignup() {
+    this.router.navigate([]);
+  }
+
+  async presentAlert(arg) {
+    const alert = await this.alertController.create({
+      header: 'Un problème est survenue..',
+      message: arg,
+      buttons: ['OK']
+    });
+
+    await alert.present();
+  }
 }
